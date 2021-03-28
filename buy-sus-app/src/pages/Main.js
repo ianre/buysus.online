14cbdb7dd664a3f0916570e71cb6e2d520bab25f
Ian Reyes <ianreyes99@gmail.com>
2021-03-27 23:23:07 -0400

    Spin Logo

--- buy-sus-app/src/pages/Main.js
+++ buy-sus-app/src/pages/Main.js
@@ -2,19 +2,25 @@ import React, { PureComponent } from "react";
 import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core";
 import logo from '../logo.svg';
+import hand from '../res/hand.png';
+import leaf from '../res/leaves.png';
 
 class Main extends PureComponent {
   state = {};
   componentDidMount() {};
   doSomething = () => {};
-  
+
   render() {
     const { classes } = this.props;
 
     return (
       <div className="App">
         <header className="App-header">
+            {/** 
           <img src={logo} className="App-logo" alt="logo" />
+          */}
+          <img src={leaf} className="Spin-move" alt="logo" />
+          <img src={hand} className="Hand" alt="logo" />
           <p>Don't be sus!</p>
           <p>Buy sustainable products</p>
           <hr></hr>
