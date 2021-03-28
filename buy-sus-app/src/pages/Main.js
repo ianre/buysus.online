62ea8de13e1ba46f03b64dc40343d176effe5bee
Ian Reyes <ianreyes99@gmail.com>
2021-03-28 01:40:58 -0400

    resizing rules on main

--- buy-sus-app/src/pages/Main.js
+++ buy-sus-app/src/pages/Main.js
@@ -64,7 +64,7 @@ export default function CenteredGrid() {
             </Typography>
           </Paper>
         </Grid>
-        <Grid item lg={4} md={12}>
+        <Grid item lg={4} md={12} xs={12} sm={12}>
           <Paper className={classes.paper}>
             <Box component="span" display="block">
               <hr color="white"></hr>
