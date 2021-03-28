d6b9269c3639dc21999c9d2b9b1e907658793a44
Ian Reyes <ianreyes99@gmail.com>
2021-03-27 22:07:04 -0400

    Added router-dom and material-ui

--- buy-sus-app/src/App.js
+++ buy-sus-app/src/App.js
@@ -1,14 +1,23 @@
+import React, { Fragment, Suspense, lazy } from "react";
+import { MuiThemeProvider, CssBaseline } from "@material-ui/core";
+import { BrowserRouter} from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+
+class App extends React.Component {
+
+  render(){
+    return(
+      <BrowserRouter>
+      <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Don't be sus! 
         </p>
+
+
         
         <p>
         Buy sustainable products
@@ -20,11 +29,13 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Reacts
         </a>
       </header>
     </div>
-  );
+      
+      </BrowserRouter>
+    )
+  }
 }
-
-export default App;
+export default App
